54acfe4824e41e3fab92a6fbb4b17aa528dd27ff
Ishraq Haider Chowdhury <ishraq.h.c@gmail.com>
2021-06-17 22:58:42 +0200

    added a component

---
+++ src/components/PokemonCard.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let name
+    export let url
+
+    const urlArray = url.split('/')
+    const indexLocation = urlArray.length - 2
+
+    const index = urlArray[indexLocation]
+    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`
+</script>
+
